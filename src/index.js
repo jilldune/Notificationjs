703a383559724c41eeddcbd7cc68652e998b2eb2
jilldune <dunejill@gmail.com>
2024-06-07 13:26:02 +0000

    Changed how the classes are exported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import Notification from "./modules/notificationClass.js";
-import Prompt from "./modules/promptClass.js";
-
-export {
-    Notification,
-    Prompt
-};
+export {default as Notification} from "./modules/notificationClass.js";
+export {default as Prompt} from "./modules/promptClass.js";
