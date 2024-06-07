a952ea3d6729eeb9b676cad5ce9d64a7c4d079aa
jilldune <dunejill@gmail.com>
2024-06-07 12:41:31 +0000

    Forgot to add index/entry file to previous commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { Notification } from "./modules/notificationClass";
-import { Prompt } from "./modules/promptClass";
+import Notification from "./modules/notificationClass.js";
+import Prompt from "./modules/promptClass.js";
 
 export {
     Notification,
     Prompt
 };
+
+// new Notification(),
+// new Prompt();
