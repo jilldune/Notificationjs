cbcf62f552c84dc41e8757f36a98be42f9dbdc7a
jilldune <dunejill@gmail.com>
2024-09-24 12:29:03 +0000

    Added UUIDV4 generator function

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -155,3 +155,11 @@ export const isObject = (obj) => {
 export const isFunction = (fn) => {
     return fn && typeof fn === "function";
 }
+
+export const generateV4UUID = () => {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
+        c => (
+            c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c/4
+        ).toString(16)
+    );
+}
